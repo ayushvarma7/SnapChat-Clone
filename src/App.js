492b7cd92a9db750f4e5c027a387e531714d49ce
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2021-03-17 23:12:49 +0530

    Image capturing implemented

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app">
     LET'S BUILD SNAPCHAT
-    <WebcamCapture/>
+    {/* <WebcamCapture/>   */}
     </div>
   );
 }
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,6 +1,6 @@
-import react, { useRef } from 'react';
+import react, { useRef, useCallback, useState } from 'react';
 import Webcam from "react-webcam";
-
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
 
 const videConstraints={
 width: 250,
@@ -12,7 +12,16 @@ facingMode: "user",
 
 function WebcamCapture() {
     const webcamRef = useRef(null);
+    const [image, setImage]= useState(null);
     
+    const capture = useCallback(() => {
+            const imageSrc= webcamRef.current.getScreenshot();
+            console.log(imageSrc);
+            setImage(imageSrc); //stores the image captured through the cam
+            //to image using the setImage hook
+        },
+        [webcamRef], //this is the dependency of this fn
+    )
     return (
         <div className="webcamCapture">
             <Webcam
@@ -23,6 +32,13 @@ function WebcamCapture() {
                 screenshotFormat="image/jpeg"
                 videoConstraints={videConstraints}
             />
+
+            <RadioButtonUncheckedIcon 
+                className="webcamCapture__button"
+                onClick={capture}
+                fontSize="large" 
+            />
+            <img src={image} alt="image captured"/> renders the image
         </div>
     )
 }
