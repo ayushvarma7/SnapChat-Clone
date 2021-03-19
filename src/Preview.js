76168de4875791729b1a91e5b4ab9fc64f548c1a
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2021-03-19 13:40:00 +0530

    Preview Interface implemented

--- src/Preview.js
+++ src/Preview.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import { resetCameraImage, selectCameraImage } from './features/cameraSlice';
-import CloseIcon from "@material-ui/icons/Close";
 import './Preview.css';
+import { AttachFile, Close, Create, Crop, MusicNote, Note, Send, TextFields, Timer } from '@material-ui/icons';
 
 function Preview() {
     const cameraImage= useSelector(selectCameraImage);
@@ -20,12 +20,28 @@ function Preview() {
     const closePreview=()=>{
         dispatch(resetCameraImage());
     }
+
+    const sendPost= ()=>{
+        
+    }
     return (
         <div className="preview">
-            <CloseIcon onClick={closePreview}
+            <Close onClick={closePreview}
             className="preview__close"/>
-            <h2>THIS IS THE PREVIEW</h2>    
+            <div className="preview__toolbarRight">
+                <TextFields/>
+                <Create/>
+                <Note/>
+                <MusicNote/>
+                <AttachFile/>
+                <Crop/>
+                <Timer/>
+            </div>
             <img src={cameraImage} />
+            <div onClick={sendPost} className="preview__footer">
+                <h2>Send now</h2>
+                <Send fontSize="small" className="preview__sendIcon" />
+            </div>
         </div>
     )
 }
