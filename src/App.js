d5786137047cdc0f6367e9504b2700d9b845c83d
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2021-03-25 00:52:47 +0530

    Major Functionality Complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import WebcamCapture from './WebcamCapture';
 import {
@@ -9,13 +9,39 @@ import {
 import Preview from './Preview';
 import Chats from './Chats';
 import ChatView from './ChatView';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/appSlice';
+import Login from './Login';
+import { auth } from './firebase';
 
 function App() {
+  const user= useSelector(selectUser);
+  const dispatch = useDispatch();
+
+
+  useEffect(()=>{
+    auth.onAuthStateChanged((authUser)  =>{ //  for any point the auth state changes, do this
+      if(authUser){
+        dispatch(  
+          login({
+          username: authUser.displayName,
+          profilePic: authUser.photoURL,
+          id: authUser.uid,
+      }))
+      }else{ //if not logged in
+        dispatch(logout());
+      }
+    })
+  }, []);
+
   return (
     <div className="app">
       {/* LET'S BUILD SNAPCHAT */}
       <Router>
-        <div className="app__body">
+      {!user ? ( // if no user then direct to login page
+        <Login/>
+      ): 
+      <div className="app__body">
           <Switch>
           <Route exact path="/chats">
               <Chats />
@@ -30,7 +56,8 @@ function App() {
               <WebcamCapture />
             </Route>
           </Switch>
-        </div>
+        </div> }
+       
       </Router>
 
     </div>
---
+++ src/Login.js
@@ -0,0 +1,33 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+import { useDispatch } from 'react-redux';
+import { login } from './features/appSlice';
+import { auth, provider } from './firebase';
+
+import './Login.css';
+function Login() {
+    const dispatch = useDispatch(); 
+    const signIn= ()=>{
+        auth.signInWithPopup(provider)
+        .then(result =>{
+            dispatch(
+                login({
+                    username: result.user.displayName,
+                    profilePic: result.user.photoURL,
+                    id: result.user.uid,
+                })
+            ) 
+        }).catch((error)=> alert(error.message));
+    }
+
+    return (
+        <div className="login">
+            <div className="login__container">
+                <img src="https://www.snapchat.com/global/social-lg.jpg" alt=""/>
+                <Button variant="outlined" onClick={signIn}>Sign In</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
